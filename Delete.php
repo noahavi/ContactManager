13b1d41920f35587217d87cf824f151bb78b28cc
theContactManager <60075208+theContactManager@users.noreply.github.com>
2020-01-24 15:15:39 -0500

    Add files via upload

---
+++ Delete.php
@@ -0,0 +1,41 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$contacts = $inData["user"];
+	$id = $inData["id"];
+
+	$conn = new mysqli("sever, username, password");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$sql = "DELETE FROM contacts WHERE id = $id";
+		if( $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+	
+	returnWithError("");
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
